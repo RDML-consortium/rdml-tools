881da4beb58208eddedc8fc6b02723ec613077f7
Andreas Untergasser <andreas@untergasser.de>
2019-01-30 09:22:03 +0100

    Experimenters can be read

--- client/src/static/js/rdml_edit.js
+++ client/src/static/js/rdml_edit.js
@@ -1,11 +1,6 @@
 const API_URL = process.env.API_URL
 const API_LINK = process.env.API_LINK
 
-$('#mainTab a').on('click', function(e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
-
 const resultLink = document.getElementById('uuid-link-box')
 
 const submitButton = document.getElementById('btn-submit')
@@ -18,26 +13,67 @@ const inputFile = document.getElementById('inputFile')
 const resultInfo = document.getElementById('result-info')
 const resultError = document.getElementById('result-error')
 const resultData = document.getElementById('result-data')
+const experimentersData = document.getElementById('experimenters-data')
 var sectionResults = document.getElementById('results')
+
+var uuid = "";
+var rdmlData = "";
 
 document.addEventListener("DOMContentLoaded", function() {
     checkForUUID();
 });
 
 function checkForUUID() {
-  var path = window.location.search; // .pathname;
-  if (path.match(/UUID=.+/)) {
-    var uuid = path.split("UUID=")[1];
-    run(uuid)
-  }
+    var path = window.location.search; // .pathname;
+    if (path.match(/UUID=.+/)) {
+        var uuid = path.split("UUID=")[1];
+        run(uuid)
+    }
 }
 
+$('#mainTab a').on('click', function(e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+
 function showExample() {
-  run("example")
+    run("example")
 }
 
 function showUpload() {
-  run("data")
+    run("data")
+}
+
+// TODO client-side validation
+function updateData() {
+    const formData = new FormData()
+    if (uuid == "sample.rdml") {
+        formData.append('showExample', 'showExample')
+    } else {
+        formData.append('uuid', uuid)
+    }
+
+    axios
+        .post(`${API_URL}/data`, formData)
+        .then(res => {
+            if (res.status === 200) {
+                rdmlData = res.data.data
+                updateAllData()
+            }
+        })
+        .catch(err => {
+            rdmlData = ""
+            let errorMessage = err
+            if (err.response) {
+                errorMessage = err.response.data.errors
+                .map(error => error.title)
+                .join('; ')
+            }
+            showElement(resultError)
+            var err = '<i class="fas fa-fire"></i>\n<span id="error-message">'
+            err += errorMessage + '</span>'
+            resultError.innerHTML = err
+        })
 }
 
 // TODO client-side validation
@@ -77,22 +113,65 @@ function run(stat) {
 
 function handleSuccess(res) {
     hideElement(resultInfo)
-    if (res.isvalid) {
-      var ret = '<p>Link to this result page:\n'
-      ret += '<a href="' + `${API_LINK}` + "edit.html?UUID=" + res.uuid + '">'
-      ret += `${API_LINK}` + "edit.html?UUID=" + res.uuid + '</a>\n'
-      ret += ' (valid for 3 days) ' + res.isvalid + '\n</p>\n<br />\n'
-      resultLink.innerHTML = ret
-      hideElement(resultError)
+    uuid = res.uuid
+    var ret = '<br /><div class="card">\n<div class="card-body">\n'
+    ret += '<h5 class="card-title">Link to this result page</h5>\n<p>'
+    ret += '<a href="' + `${API_LINK}` + "edit.html?UUID=" + res.uuid + '">'
+    ret += `${API_LINK}` + "edit.html?UUID=" + res.uuid + '</a> (valid for 3 days)\n</p>\n'
+    ret += '<p>File is '
+    if (!(res.isvalid)) {
+        ret += 'not '
+        err = '<i class="fas fa-fire"></i>\n<span id="error-message">'
+        resultError.innerHTML = err + 'Error: Uploaded file is not valid RDML!</span>'
+        showElement(resultError)
     } else {
-      resultLink.innerHTML = ""
-      var ret = '<i class="fas fa-fire"></i>\nNot a valid RDML file:\n'
-      ret += '<a href="' + `${API_LINK}` + "validate.html?UUID=" + res.uuid + '" target="_blank">'
-      ret += `${API_LINK}` + "validate.html?UUID=" + res.uuid + '</a> (click for validation results)\n'
-      hideElement(resultInfo)
-      showElement(resultError)
-      resultError.innerHTML = ret
+        hideElement(resultError)
     }
+    ret += 'valid RDML! Click here for more information:<br />'
+    ret += '<a href="' + `${API_LINK}` + "validate.html?UUID=" + res.uuid + '" target="_blank">'
+    ret += `${API_LINK}` + "validate.html?UUID=" + res.uuid + '</a> (valid for 3 days)\n<br />\n'
+    ret += '</p>\n</div>\n</div>\n'
+    resultLink.innerHTML = ret
+    updateData()
+}
+
+function updateAllData() {
+    if (!(rdmlData.hasOwnProperty("rdml"))) {
+        deleteAllData()
+        return
+    }
+    ret = ''
+    var exp = rdmlData.rdml.experimenters;
+    for (var i = 0; i < exp.length; i++) {
+        ret += '<br /><div class="card">\n<div class="card-body">\n'
+        ret += '<h5 class="card-title">Experimenter ID: ' + exp[i].id + '</h5>\n<p>'
+        ret += '<table style="width:100%;">'
+        ret += '  <tr>\n    <td style="width:20%;">Name:</td>\n'
+        ret += '    <td style="width:80%">\n'+ exp[i].lastName + ', ' + exp[i].firstName + '</td>\n'
+        ret += '  </tr>'
+        if (exp[i].hasOwnProperty("email")) {
+          ret += '  <tr>\n    <td style="width:20%;">E-Mail:</td>\n'
+          ret += '    <td style="width:80%">\n'+ exp[i].email + '</td>\n'
+          ret += '  </tr>'
+        }
+        if (exp[i].hasOwnProperty("labName")) {
+          ret += '  <tr>\n    <td style="width:20%;">Lab Name:</td>\n'
+          ret += '    <td style="width:80%">\n'+ exp[i].labName + '</td>\n'
+          ret += '  </tr>'
+        }
+        if (exp[i].hasOwnProperty("labAddress")) {
+          ret += '  <tr>\n    <td style="width:20%;">Lab Address:</td>\n'
+          ret += '    <td style="width:80%">\n'+ exp[i].labAddress + '</td>\n'
+          ret += '  </tr>'
+        }
+        ret += '</table></p>\n</div>\n</div>\n'
+    }
+    experimentersData.innerHTML = ret
+
+}
+
+function deleteAllData() {
+    experimentersData.innerHTML = ""
 }
 
 function showElement(element) {
--- server/server.py
+++ server/server.py
@@ -3,10 +3,8 @@
 import os
 import uuid
 import re
-import subprocess
 import argparse
 import json
-from subprocess import call
 from flask import Flask, send_file, flash, send_from_directory, request, redirect, url_for, jsonify
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
@@ -25,9 +23,12 @@ app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024   # maximum of 32MB
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in set(['rdml', 'rdm', 'xml'])
 
+
 uuid_re = re.compile(r'(^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})-{0,1}([ap]{0,1})([cj]{0,1})$')
+
+
 def is_valid_uuid(s):
-   return uuid_re.match(s) is not None
+    return uuid_re.match(s) is not None
 
 
 @app.route('/api/v1/validate', methods=['POST'])
@@ -71,8 +72,8 @@ def validate_file():
         # Run RDML-Python
         rd = rdml.Rdml()
         ret = rd.validate(fexpname)
-        return jsonify(data = {"table": ret, "uuid": uuidstr})
-    return jsonify(errors = [{"title": "Error in handling POST request!"}]), 400
+        return jsonify(data={"table": ret, "uuid": uuidstr})
+    return jsonify(errors=[{"title": "Error in handling POST request!"}]), 400
 
 
 @app.route('/api/v1/upload', methods=['POST'])
@@ -116,8 +117,41 @@ def upload_file():
         # Return the uuid
         rd = rdml.Rdml()
         ret = rd.isvalid(fexpname)
-        return jsonify(data = {"isvalid": ret, "uuid": uuidstr})
-    return jsonify(errors = [{"title": "Error in handling POST request!"}]), 400
+        return jsonify(data={"isvalid": ret, "uuid": uuidstr})
+    return jsonify(errors=[{"title": "Error in handling POST request!"}]), 400
+
+
+@app.route('/api/v1/data', methods=['POST'])
+def handle_data():
+    if request.method == 'POST':
+        if 'showExample' in request.form.keys():
+            fexpname = os.path.join(RDMLWS, "sample.rdml")
+            uuidstr = "sample.rdml"
+        elif 'uuid' in request.form.keys():
+            uuidstr = request.form['uuid']
+            if uuidstr == "sample.rdml":
+                fexpname = os.path.join(RDMLWS, "sample.rdml")
+            else:
+                if not is_valid_uuid(uuidstr):
+                    return jsonify(errors=[{"title": "Invalid UUID - UUID link outdated or invalid!"}]), 400
+                sf = os.path.join(app.config['UPLOAD_FOLDER'], uuidstr[0:2])
+                if not os.path.exists(sf):
+                    return jsonify(errors=[{"title": "Invalid path - UUID link outdated or invalid!"}]), 400
+                fname = "rdml_" + uuidstr + ".rdml";
+                if not allowed_file(fname):
+                    return jsonify(errors=[{"title": "Invalid filename - UUID link outdated or invalid!"}]), 400
+                fexpname = os.path.join(sf, fname)
+                if not os.path.isfile(fexpname):
+                    return jsonify(errors=[{"title": "Invalid file - UUID Link outdated or invalid!"}]), 400
+        else:
+            return jsonify(errors=[{"title": "Upload RDML file first!"}]), 400
+
+        # Run RDML-Python
+        rd = rdml.Rdml(fexpname)
+        ret = rd.tojson()
+        rd.save("nnnn.rdml")
+        return jsonify(data=ret)
+    return jsonify(errors=[{"title": "Error in handling POST request!"}]), 400
 
 
 if __name__ == '__main__':
